1bd50bf94b624c2118966993f81cfff60c482a5a
8dcc <8dcc.git@gmail.com>
2023-01-18 09:29:00 +0100

    Add debian deps script

---
+++ debian-deps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Dependencies for debian/ubuntu
+sudo apt install -y \
+    build-essential \
+    bison           \
+    flex            \
+    libgmp3-dev     \
+    libmpc-dev      \
+    libmpfr-dev     \
+    texinfo         \
+    libisl-dev
+
