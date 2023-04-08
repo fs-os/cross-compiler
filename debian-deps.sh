eaf53625ac2b8ba431d97ee5fed67b3b2c55f113
8dcc <8dcc.git@gmail.com>
2023-04-08 21:33:50 +0200

    Add sudo checks for debian-deps.sh

--- debian-deps.sh
+++ debian-deps.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-# Run as root!
+ADMIN_CMD=""
+
+# If we are root, run without sudo. Debian docker container doesn't include sudo
 if [ $(whoami) != "root" ]; then
-    echo "You need to be root!"
-    exit 1
+    # If we are not root but we have sudo installed, use it when calling apt
+    if [ $(command -v sudo) ]; then
+        ADMIN_CMD="sudo"
+    else
+        # If we are not root and we don't have sudo, exit
+        echo "You need to be root!"
+        exit 1
+    fi
 fi
 
 # Dependencies for debian/ubuntu
 # Nasm is not required for the cross-compiler but its required for fs-os
-apt install -y      \
-    curl            \
-    build-essential \
-    bison           \
-    flex            \
-    libgmp3-dev     \
-    libmpc-dev      \
-    libmpfr-dev     \
-    texinfo         \
-    libisl-dev      \
+$ADMIN_CMD apt install -y \
+    curl                  \
+    build-essential       \
+    bison                 \
+    flex                  \
+    libgmp3-dev           \
+    libmpc-dev            \
+    libmpfr-dev           \
+    texinfo               \
+    libisl-dev            \
     nasm
 
